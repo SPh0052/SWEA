07fd5bac311a694d1a4076c908d8a61f9114778d
SPh0052 <137913404+SPh0052@users.noreply.github.com>
2025-08-16 19:14:57 +0900

    [D1] Title: 연속한 1의 개수, Time: 68 ms, Memory: 56,448 KB -BaekjoonHub

---
+++ SWEA/D1/9386. 연속한 1의 개수/연속한 1의 개수.py
@@ -0,0 +1,22 @@
+#N개의 0과 1로 이루어진 수열
+#"연속한" 1의 개수 중 최댓값 출력
+# 1시작 -> 세다가 0 만나면 다시 초기화
+
+T = int(input())
+for tc in range(1,T+1):
+    N = int(input())
+    arr = list(input())
+    result = []
+    cnt_1 = 0
+    for i in range(N):
+        if arr[i] == '1':
+            cnt_1 += 1
+
+        else: #0이 출현하면
+            result.append(cnt_1) #1이 마지막으로 끝났을 때는?
+            cnt_1 = 0
+
+    result.append(cnt_1) #1이 마지막으로 끝났을 때도 포함
+
+
+    print(f'#{tc} {max(result)}')
