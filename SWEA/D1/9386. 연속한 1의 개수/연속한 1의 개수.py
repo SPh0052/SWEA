885763c4d692d7fc9a76cdda1824dba286218b7c
SPh0052 <137913404+SPh0052@users.noreply.github.com>
2025-08-16 19:25:41 +0900

    Update 연속한 1의 개수.py

--- SWEA/D1/9386. 연속한 1의 개수/연속한 1의 개수.py
+++ SWEA/D1/9386. 연속한 1의 개수/연속한 1의 개수.py
@@ -11,6 +11,8 @@ for tc in range(1,T+1):
     for i in range(N):
         if arr[i] == '1':
             cnt_1 += 1
+            #if max_v < c:
+                #max_v = c #c를 하나씩 더 해서 그 때마다 max_v와 비교하기. 그리고 제일 큰 값 출력
 
         else: #0이 출현하면
             result.append(cnt_1) #1이 마지막으로 끝났을 때는?
