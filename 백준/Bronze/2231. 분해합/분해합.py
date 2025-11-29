0ea57947c9ec0149d6ab83bd5dfe140893374afe
SPh0052 <137913404+SPh0052@users.noreply.github.com>
2025-11-29 23:24:04 +0900

    [Bronze II] Title: 분해합, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,15 +1,7 @@
-N = int(input())
-
-# 어차피 다 돌릴 거라면 생성자 1부터 찾기.
-# 최솟값이 바로 나오니까.
-for i in range(1,N+1):
-    # i의 각 자리수 더하기.
-    cons = sum(map(int,str(i))) + i
-    
-    if cons == N:
+n = int(input())
+for i in range(max(1, n - len(str(n)) * 9), n):
+    if i + sum(map(int, str(i))) == n:
         print(i)
         break
-    
-    # 생성자 i ==N 이라는 것은 생성자가 없다는 뜻    
-    if i == N:
-        print(0)
+else:
+    print(0)
