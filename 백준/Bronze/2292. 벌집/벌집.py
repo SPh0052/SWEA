a3ead2b9ddb67655200ca93fa4ce2db7eba57f2e
SPh0052 <137913404+SPh0052@users.noreply.github.com>
2025-11-30 11:43:48 +0900

    [Bronze II] Title: 벌집, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.py
+++ 백준/Bronze/2292. 벌집/벌집.py
@@ -1,4 +1,26 @@
-import math
-X = int(input())
+'''
+3(n-1)*(n-2)+2 <= x <= 3n(n-1)+1
+n은 층이고,
+x는 벌집 넘버이다.
+'''
+'''
+#1트
+print(int(((X-1)/3 +1/4)**0.5 + 1/2 +1))
+이 식으로 썼다면 (X-1)/3 +1/4)**0.5 + 1/2 이 식이 정수일 때 오류가 났겠군.
+math.ceil() : 올림(천장)
+math.floor() : 내림
+math.round() : 반올림
+'''
+# import math
+# X = int(input())
+# 
+# print(math.ceil(((X-1)/3 +1/4)**0.5 + 1/2))
 
-print(math.ceil(((X-1)/3 +1/4)**0.5 + 1/2))
+n = int(input())
+
+nums_pileup = 1  # 벌집의 개수, 1개부터 시작
+cnt = 1
+while n > nums_pileup :
+    nums_pileup += 6 * cnt  # 벌집이 6의 배수로 증가
+    cnt += 1  # 반복문을 반복하는 횟수
+print(cnt)
