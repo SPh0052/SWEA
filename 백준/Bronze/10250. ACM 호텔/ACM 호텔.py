2e06dd691a1a6ddad7cfe66f3eeb5fb46fc9a2d2
SPh0052 <137913404+SPh0052@users.noreply.github.com>
2025-12-06 22:29:13 +0900

    [Bronze III] Title: ACM 호텔, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -1,34 +1,58 @@
 '''
-정문으로부터 거리가 가장 짧도록 방 배정
-정문 : 일층 엘리베이터 바로 앞
-정문에서 엘리베이터까지의 거리 무시
-모든 인접한 두 방사이의 거리 동일
-호텔의 정면 쪽에만 방이 있음
-W : 방 개수
-H : 층
+T = int(input())
 
-층수 엘리베이터에서 세었을 때의 번호
-거리 같을 때는 아래층의 방 선호
-Y XX
-YY XX
+def answer():
+    for _ in range(T):
+        H, W, N = map(int, input().split())
 
-N번째로 도착한 손님에게 몇 번 방?
-H다 돌고
-그다음 W
+        # N번째 손님의 층과 호수 계산
+        floor = N % H
+        room = N // H + 1
+
+        if floor == 0:   # H의 배수인 경우(꼭대기층)
+            floor = H
+            room = N // H
+
+        # 방 번호 출력 (호수는 항상 2자리)
+        print(f"{floor}{room:02d}")
+
+answer()
+
+
 '''
 
 T = int(input())
 
-for _ in range(T):
-    H, W, N = map(int, input().split())
-    cnt  = 0
+def answer():
+    for _ in range(T):
+        H, W, N = map(int, input().split())
+        cnt  = 0
+        done = False
 
-    for width in range(1, W+1):
-        for height in range(1, H+1):
-            cnt +=1
-            if cnt == N:
-                if width < 10:
-                    print(f'{height}0{width}')
-                else:
-                    print(f'{height}{width}')
+        for width in range(1, W+1):
+            for height in range(1, H+1):
+                cnt +=1
+                if cnt == N:
+                    #{값:형식}={height:02}
+                    # 0 : 빈자리는 0으로 채워라
+                    # 전체 길이는 2자리로 맞춰라
+                    print(f'{height}{width:02}')
+                    done = True
+                    break
+            if done:
                 break
+
+answer()
+
+
+
+
+                # 이 코드에서의 문제점
+                # 1. break 문은 가장 인접한 반복문 한 개만 중지시킨다.
+                # 그래서 break를 해도 for width 쪽은 다 돈다.
+                # 2. f-string 써서 더 간단하게 하기.
+                # if width < 10:
+                #     print(f'{height}0{width}')
+                # else:
+                #     print(f'{height}{width}')
+                #break
