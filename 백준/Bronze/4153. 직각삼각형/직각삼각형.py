75480237b72fb60222ae923d6a776a88326a3d5f
SPh0052 <137913404+SPh0052@users.noreply.github.com>
2025-12-18 17:53:27 +0900

    [Bronze III] Title: 직각삼각형, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.py
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -1,15 +1,10 @@
-'''
-주어진 세 변의 길이로 삼각형이 직각인지 구분하시오
-맞다면 right, 아니라면 wrong
-'''
 while True:
-    tri_list = list(map(int, input().split()))
-    if sum(tri_list) == 0:
+    tri_len = list(map(int, input().split()))
+
+    tri_len.sort()
+    if sum(tri_len) == 0:
         break
-    tri_list.sort()
-
-    if tri_list[2]**2 == tri_list[0]**2 + tri_list[1]**2:
-        print("right")
+    if tri_len[2]**2 == tri_len[0]**2 + tri_len[1]**2:
+        print('right')
     else:
-        print("wrong")
-
+        print('wrong')
